9ec114f87f46dfd062239380d28fe958d61ff12f
Khrongpop Phonngam <mr.khrongpop@gmail.com>
2018-11-25 02:36:00 +0700

    revers

--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
-	"github.com/labstack/echo"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	port := os.Getenv("PORT")
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", homepage)
+	mux.HandleFunc("/greets/{name}", greeting)
+	fmt.Println("Start Serve http://localhost:8080/ ")
+	// http.ListenAndServe(":8080", mux)
+	http.ListenAndServe(":"+port, mux)
+}
+
+func homepage(w http.ResponseWriter, req *http.Request) {
+	// The "/" pattern matches everything, so we need to check
+	// that we're at the root here.
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+	w.Header().Set("Content-type", "appliation/json")
+	fmt.Fprintf(w, `{"message":"wellcome to homepage"}`)
+}
+
+func greeting(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	name := vars["name"]
+
+	w.Header().Set("Content-type", "appliation/json")
+	fmt.Fprintf(w, `{"message":"Hello %s"}`, name)
 }
